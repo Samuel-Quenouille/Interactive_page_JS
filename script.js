5af19b9167053bad01234c2c69abb6a55a1e3582
Samuel-Quenouille <quenouille_samuel@yahoo.com>
2023-05-29 18:14:17 +0200

    modifs 2

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-//Fonctionnalité 1 :
+//Fonctionnalité 1 : Afficher le mot "clique" en console.
 let footer = document.querySelector('footer');
 footer.addEventListener('click', function() {
     console.log('clique');
 });
 
-//Fonctionnalité 1-bis :
+//Fonctionnalité 1-bis : Afficher en console "clic numéro x" avec x qui commence à 1 et s'incrémente de +1 à chaque clic
 let footerBis = document.querySelector('footer');
 let clickCount = 0;
 footerBis.addEventListener('click', function() {
@@ -12,21 +12,21 @@ footerBis.addEventListener('click', function() {
     console.log('clic numéro' + clickCount);
 });
 
-//Fonctionnalité 2 :
+//Fonctionnalité 2 : Faire fonctionner ce satané "Hamburger Menu"
 let navbar = document.querySelector('#navbarHeader')
 let button = document.querySelector("body > header > div.navbar.navbar-dark.bg-dark.box-shadow > div > button")
 button.addEventListener('click', function() {
     navbar.classList.toggle("collapse")
 });
 
-//Fonctionnalité 3 :
+//Fonctionnalité 3 : Si on clique sur le bouton "Edit" de la première card, le texte de la card va se mettre en rouge de façon irréversible (sauf si on recharge la page).
 let card = document.querySelector("body > main > div > div > div > div:nth-child(1) > div > div")
 let edit = document.querySelector("body > main > div > div > div > div:nth-child(1) > div > div > div > div > button.btn.btn-sm.btn-outline-secondary")
 card.addEventListener('click', function() {
     this.style.color = 'red';
 });
 
-//Fonctionnalité 4 :
+//Fonctionnalité 4 : Si on clique sur le bouton "Edit" de la deuxième card, le texte de la card va se mettre en vert. Si on re-clique dessus, il redevient comme avant !
 let secondCard = document.querySelector("body > main > div > div > div > div:nth-child(2) > div > div")
 let secondEdit = document.querySelector("body > main > div > div > div > div:nth-child(2) > div > div > div > div > button.btn.btn-sm.btn-outline-secondary")
 secondCard.addEventListener('click', function() {
@@ -37,7 +37,7 @@ secondCard.addEventListener('click', function() {
     }
 });
 
-//Fonctionnalité 5 :
+//Fonctionnalité 5 : Si un utilisateur double clique sur la navbar en haut, tout Bootstrap disparaît et la page s'affiche comme si on avait oublié de mettre le CDN qui la relie au fichier CSS. Si possible, rends cette fonctionnalité réversible (un nouveau double-clic fait tout revenir à la normale).
 let navbarBis = document.querySelector("body > header > div.navbar.navbar-dark.bg-dark.box-shadow")
 let bootstrap = document.querySelector("head > link")
 navbarBis.addEventListener('dblclick', function() {
